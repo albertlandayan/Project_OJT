cff51251972cc11f558ef4dea4a515baafc391da
Albert Landayan <albert.landayan@kambi.com>
2017-11-10 13:49:51 +0800

    Latest build

--- src/main/java/InventorySystem/Controllers/AssetsController.java
+++ src/main/java/InventorySystem/Controllers/AssetsController.java
@@ -1,10 +1,7 @@
 package InventorySystem.Controllers;
 
 import InventorySystem.Database.Database;
-import InventorySystem.Models.Assets;
-import InventorySystem.Models.DeleteAssets;
-import InventorySystem.Models.GetAssetsList;
-import InventorySystem.Models.GetTotalQuant;
+import InventorySystem.Models.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -35,21 +32,21 @@ public class AssetsController {
 
     //SHOULD GET SPECIFIC INVENTORY ITEMS AND BE DISPLAYED SORTED BY WHAT YOU WANT (invoice,permit,etc).
     //Gonna make a document for which parameters are needed for specific sorting
-    @RequestMapping(value = "query/searchAsset/products/{param}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "query/searchAsset/products/\"{param}\"", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     GetAssetsList searchAssetPr(@PathVariable String param) throws SQLException {
         Database db = new Database();
         return db.SearchParams(param, 3);
     }
 
-    @RequestMapping(value = "query/searchAsset/invDate/{param}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "query/searchAsset/invDate/\"{param}\"", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     GetAssetsList searchAssetInvDate(@PathVariable String param) throws SQLException {
         Database db = new Database();
         return db.SearchParams(param, 1);
     }
 
-    @RequestMapping(value = "query/searchAsset/perDate/{param}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "query/searchAsset/perDate/\"{param}\"", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     GetAssetsList searchAssetPerDate(@PathVariable String param) throws SQLException {
         Database db = new Database();
@@ -61,9 +58,13 @@ public class AssetsController {
     @RequestMapping(value = "query/addItem", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public @ResponseBody
-    void addItem(@RequestBody Assets params) throws SQLException {
+    Response addItem(@RequestBody Assets params) throws SQLException {
         Database db = new Database();
         db.CreateEntry(params);
+        Response res = new Response();
+        res.setStatus("200");
+        res.setMessage("Success!");
+        return res;
 
     }
 
@@ -71,18 +72,27 @@ public class AssetsController {
     @RequestMapping(value = "query/deleteItem", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public @ResponseBody
-    void deleteItem(@RequestBody DeleteAssets params) throws SQLException {
+    Response deleteItem(@RequestBody DeleteAssets params) throws SQLException {
         Database db = new Database();
+        //Responses here are temporary.
+        Response res = new Response();
+        res.setStatus("200");
+        res.setMessage("Success!");
         db.DeleteEntry(params.getSerialNum(), params.getSupName());
+        return res;
     }
 
     //SHOULD TAKE REQUEST, CONVERT TO Assets Model from JSON, ACCESS DB AND UPDATE ITEM FROM JSON POST
     @RequestMapping(value = "query/updateItem", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public @ResponseBody
-    void updateItem(@RequestBody Assets params) throws SQLException {
+    Response updateItem(@RequestBody Assets params) throws SQLException {
         Database db = new Database();
+        Response res = new Response();
+        res.setStatus("200");
+        res.setMessage("Success!");
         db.UpdateItem(params);
+        return res;
     }
 
     //SHOULD GET THE TOTAL QUANTITY OF THE ITEMS IN THE INVENTORY
---
+++ src/main/java/InventorySystem/Models/Response.java
@@ -0,0 +1,25 @@
+package InventorySystem.Models;
+
+public class Response {
+
+    String Status;
+    String Message;
+
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
+
+}
